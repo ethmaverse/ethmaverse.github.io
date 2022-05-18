1cacc993eb04f2bea777990602eed83fc6096a3e
ethmaverse <105800411+ethmaverse@users.noreply.github.com>
2022-05-18 22:36:38 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-var tombolMenu = document.getElementsByClassName('tombol-menu')[0];
-var menu = document.getElementsByClassName('menu')[0];
-
-tombolMenu.onclick = function() {
-    menu.classList.toggle('active');
-}
-
-menu.onclick = function() {
-    menu.classList.toggle('active');
-}
+$('#toggle').click(function() {
+   $(this).toggleClass('active');
+   $('#overlay').toggleClass('open');
+  });
